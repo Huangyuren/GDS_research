f216a512b93f49dfb606564d4bd05cec3aff64f7
Huangyuren <forautodesk1@gmail.com>
2020-11-22 00:20:07 +0800

    [Modify] add argument parser

--- genFakeImages.py
+++ genFakeImages.py
@@ -1,6 +1,13 @@
 import cv2
 import numpy as np
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--row", type=int, default=2048)
+parser.add_argument("--col", type=int, default=2048)
+parser.add_argument("--iteration", type=int, help="How many images to generate", default=1000)
+parser.add_argument("--path", type=str, help="image dir to store", required=True)
+args = parser.parse_args()
 
 def create_blank(width, height, rgb):
     image = np.zeros((height, width, 3), np.uint8)
@@ -11,11 +18,12 @@ def create_blank(width, height, rgb):
 
 
 # Create new blank 300x300 red image
-width = 2048
-height = 2048
-pathDir = "./data/2kResolution/"
+width = args.row
+height = args.col
+pathDir = args.path
+iteration = args.iteration
 blue = (0, 0, 255)
-for i in range(1000):
+for i in range(iteration):
     image = create_blank(width, height, blue)
     cv2.imwrite(pathDir+"image_"+str(i)+".jpg", image)
     print("Index: ", i)
