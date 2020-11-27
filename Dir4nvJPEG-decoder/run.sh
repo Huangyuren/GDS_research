bc1c26f2b11114f11b298b7981328d1bc52a7891
Huangyuren <forautodesk1@gmail.com>
2020-11-27 13:57:14 +0800

    [Modify] code structure changed

--- Dir4nvJPEG-decoder/run.sh
+++ Dir4nvJPEG-decoder/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-g++ -m64 ./mynvJpegDecoder.cpp -I../include -lnvjpeg -L../lib64 -I/usr/local/cuda-11.0/include -ldl -lrt -pthread -lcudart -L/usr/local/cuda-11.0/lib64 -Wl,-rpath=../lib64 -Wl,-rpath=/usr/local/cuda-11.0/lib64 -o test_out
+file_name="mynvJpegDecoder"
+VAR1="${file_name}.cpp"
+g++ -m64 ./$VAR1 -I../include -lnvjpeg -L../lib64 -I/usr/local/cuda-11.0/include -ldl -lrt -pthread -lcudart -L/usr/local/cuda-11.0/lib64 -Wl,-rpath=../lib64 -Wl,-rpath=/usr/local/cuda-11.0/lib64 -o $file_name
