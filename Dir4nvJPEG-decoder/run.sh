efba6f1ecb42d477d37fb3ffbb02a96f784ae2bd
Huangyuren <forautodesk1@gmail.com>
2020-12-17 14:58:14 +0800

    modify mynvjpeg name

--- Dir4nvJPEG-decoder/run.sh
+++ Dir4nvJPEG-decoder/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-file_name="mynvJpegDecoder"
+file_name="mynvjpegDecoder"
 VAR1="${file_name}.cpp"
-g++ -m64 ./$VAR1 -I../include -lnvjpeg -L../lib64 -I/usr/local/cuda-11.0/include -ldl -lrt -pthread -lcudart -L/usr/local/cuda-11.0/lib64 -Wl,-rpath=../lib64 -Wl,-rpath=/usr/local/cuda-11.0/lib64 -o $file_name
+g++ -m64 ./src/$VAR1 -I../include -lnvjpeg -L../lib64 -I/usr/local/cuda-11.0/include -ldl -lrt -pthread -lcudart -L/usr/local/cuda-11.0/lib64 -Wl,-rpath=../lib64 -Wl,-rpath=/usr/local/cuda-11.0/lib64 -o $file_name
